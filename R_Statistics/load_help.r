c748fd19dba7d28374fae65b207b2043694d1fcd
Justin <lego.man.123@hotmail.com>
2023-04-07 09:45:30 -0600

    Basic R visualizations and load data

---
+++ R_Statistics/load_help.r
@@ -0,0 +1,14 @@
+multiplesheets <- function(fname) {
+  
+  # getting info about all excel sheets
+  sheets <- readxl::excel_sheets(fname)
+  tibble <- lapply(sheets, function(x) readxl::read_excel(fname, sheet = x))
+  data_frame <- lapply(tibble, as.data.frame)
+  
+  # assigning names to data frames
+  names(data_frame) <- sheets
+  
+  # # print data frame
+  # print(data_frame)
+  return (data_frame)
+}
